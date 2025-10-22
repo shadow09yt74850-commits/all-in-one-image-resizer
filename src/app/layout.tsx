359738be79484a500645a1d5cf799ddd9346e32f
Shadow_Strike <shadow09yt74850@gmail.com>
2025-10-22 22:25:13 +0530

    Add Google verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,4 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>{children}</body>
     </html>
   );
+  <head>
+    <meta name="google-site-verification" content="2uAkDfXAO16opMrA1-ds2pPWbVEysp0pyuYELBjp4Rs" />
+  </head>
 }
